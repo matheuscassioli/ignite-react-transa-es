bcd35a859836864e080308c5bf1f7afa29bc892b
Matheus Cassioli <matheuscassioli@hotmail.com>
2024-11-13 20:07:58 -0300

    feature(lista) : renderizando lista + sumário

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,37 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable } from "./styles";
 
 const Transactions = () => {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.500,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.500,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Alimentação</td>
+              <td>- R$ 12.500,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
